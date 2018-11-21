9f2cdd6845e66cdf6f6e71a3a6470542e20adf25
Seres István András <seresistvanandras@gmail.com>
2018-11-21 15:30:48 +0100

    New shuffle as an event for clients

--- contracts/MixEth.sol
+++ contracts/MixEth.sol
@@ -22,7 +22,7 @@ contract MixEth is ERC223ReceivingContract {
 
   uint256 public amt = 1000000000000000000; //1 ether in wei, the amount of ether to be mixed;
   uint256 public shufflingDeposit = 1000000000000000000; // 1 ether, TBD
-  mapping(address => bool) public shuffleRound; // we only store the parity of the shuffle round! false -> 0, true -> 1
+  mapping(address => Shuffler) public shuffleRound; // we only store the parity of the shuffle round! false -> 0, true -> 1 and additionally the shuffler's address
   mapping(address => Status) public shufflers;
   mapping(address => mapping(bool => Shuffle)) public Shuffles;
 
@@ -34,20 +34,27 @@ contract MixEth is ERC223ReceivingContract {
     address shuffler;
   }
 
+  struct Shuffler {
+    bool turn;
+    address shufflerAddress;
+  }
+
   struct Status {
     bool alreadyShuffled;
     bool slashed;
   }
 
+  event newShuffle(address indexed token, bool actualRound, address shuffler, uint256[] shuffle);
+
   function () public {
-        revert();
+    revert();
   }
 
   function depositEther(uint256 initPubKeyX, uint256 initPubKeyY) public payable {
     require(msg.value == amt, "Ether denomination is not correct!");
     require(EC.onCurve([initPubKeyX, initPubKeyY]), "Invalid public key!");
-    Shuffles[0x0][shuffleRound[0x0]].shuffle.push(initPubKeyX);
-    Shuffles[0x0][shuffleRound[0x0]].shuffle.push(initPubKeyY);
+    Shuffles[0x0][shuffleRound[0x0].turn].shuffle.push(initPubKeyX);
+    Shuffles[0x0][shuffleRound[0x0].turn].shuffle.push(initPubKeyY);
   }
 
   /*
@@ -61,8 +68,8 @@ contract MixEth is ERC223ReceivingContract {
     }
     require(token != 0 && codeLength > 0);
     require(EC.onCurve([initPubKeyX, initPubKeyY]), "Invalid public key!");
-    Shuffles[token][shuffleRound[token]].shuffle.push(initPubKeyX);
-    Shuffles[token][shuffleRound[token]].shuffle.push(initPubKeyY);
+    Shuffles[token][shuffleRound[token].turn].shuffle.push(initPubKeyX);
+    Shuffles[token][shuffleRound[token].turn].shuffle.push(initPubKeyY);
 
     ERC20Compatible untrustedErc20Token = ERC20Compatible(token);
     untrustedErc20Token.transferFrom(msg.sender, this, 100);
@@ -71,9 +78,11 @@ contract MixEth is ERC223ReceivingContract {
   function uploadShuffle(address token, uint256[] _shuffle) public payable {
     require(msg.value == shufflingDeposit, "Invalid shuffler deposit amount!");
     require(!shufflers[msg.sender].alreadyShuffled, "Shuffler is not allowed to shuffle more than once!");
-    Shuffles[token][!shuffleRound[token]] = Shuffle(_shuffle, msg.sender);
-    shuffleRound[token] = !shuffleRound[token];
+    Shuffles[token][!shuffleRound[token].turn] = Shuffle(_shuffle, msg.sender);
+    shuffleRound[token].turn = !shuffleRound[token].turn;
     shufflers[msg.sender].alreadyShuffled = true; // a receiver can only shuffle once
+
+    emit newShuffle(token, !shuffleRound[token].turn, msg.sender, _shuffle);
   }
 
   /*
@@ -96,7 +105,7 @@ contract MixEth is ERC223ReceivingContract {
     }
     require(!includedInShuffle && ChaumPedersenVerifier.verifyChaumPedersen(proofTranscript), "Chaum-Pedersen Proof not verified");
     shufflers[Shuffles[token][!round].shuffler].slashed = true;
-    shuffleRound[token] = !shuffleRound[token];
+    shuffleRound[token].turn = !shuffleRound[token].turn;
     //delete the shuffle entirely
     for(i=0; i < (currentShuffleLength-2)/2; i++) {
       Shuffles[token][!round].shuffle[2*i] = 0;
@@ -120,28 +129,28 @@ contract MixEth is ERC223ReceivingContract {
    }
 
    function withdrawChecks(uint256[12] sig, address token, uint256 indexInShuffle) internal {
-     uint256 currentShuffleLength = Shuffles[token][shuffleRound[token]].shuffle.length;
-     require(Shuffles[token][shuffleRound[token]].shuffle[indexInShuffle] == sig[2] && Shuffles[token][shuffleRound[token]].shuffle[indexInShuffle+1] == sig[3], "Your public key is not included in the final shuffle!"); //public key is included in Shuffled
-     require(Shuffles[token][shuffleRound[token]].shuffle[currentShuffleLength-2] == sig[0] && Shuffles[token][shuffleRound[token]].shuffle[currentShuffleLength-1] == sig[1], "Your signature is using a wrong generator!"); //shuffling accumulated constant is correct
+     uint256 currentShuffleLength = Shuffles[token][shuffleRound[token].turn].shuffle.length;
+     require(Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle] == sig[2] && Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle+1] == sig[3], "Your public key is not included in the final shuffle!"); //public key is included in Shuffled
+     require(Shuffles[token][shuffleRound[token].turn].shuffle[currentShuffleLength-2] == sig[0] && Shuffles[token][shuffleRound[token].turn].shuffle[currentShuffleLength-1] == sig[1], "Your signature is using a wrong generator!"); //shuffling accumulated constant is correct
      require(sig[4] == uint(sha3(msg.sender,sig[2],sig[3])), "Signed an invalid message!"); //this check is needed to deter front-running attacks
      require(ECDSAGeneralized.verify(sig), "Your signature is not verified!");
-     Shuffles[token][shuffleRound[token]].shuffle[indexInShuffle] = 0;
-     Shuffles[token][shuffleRound[token]].shuffle[indexInShuffle+1] = 0;
+     Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle] = 0;
+     Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle+1] = 0;
    }
 
   function withdrawDeposit() public onlyShuffler onlyHonestShuffler {
-      msg.sender.transfer(shufflingDeposit);
-      shufflers[msg.sender].slashed = true; //we only allow to withdraw shuffler deposits once
+    shufflers[msg.sender].slashed = true; //we only allow to withdraw shuffler deposits once
+    msg.sender.transfer(shufflingDeposit);
   }
 
   // Shuffled pubKeys in shuffleRound
-    function getShuffle(address token, bool round) public view returns (uint256[] pubKeys) {
-        return Shuffles[token][round].shuffle;
-    }
+  function getShuffle(address token, bool round) public view returns (uint256[] pubKeys) {
+    return Shuffles[token][round].shuffle;
+  }
 
   modifier onlyShuffler() {
-        require(shufflers[msg.sender].alreadyShuffled, "You are not authorized to shuffle");
-        _;
+    require(shufflers[msg.sender].alreadyShuffled, "You are not authorized to shuffle");
+    _;
   }
 
   modifier onlyHonestShuffler() {
