64e1ff94b1db17a143660736bb2a265fa573c75c
Seres István András <seresistvanandras@gmail.com>
2018-11-21 17:45:04 +0100

    Shuffles in log, constant challenge size

--- contracts/MixEth.sol
+++ contracts/MixEth.sol
@@ -13,8 +13,8 @@ import './ERC223ReceivingContract.sol';
  * namely, transferFrom() (used to do a Deposit), and transfer() (used to do a withdrawal)
 **/
 contract ERC20Compatible {
-    function transferFrom(address from, address to, uint256 value) public;
-    function transfer(address to, uint256 value) public;
+  function transferFrom(address from, address to, uint256 value) public;
+  function transfer(address to, uint256 value) public;
 }
 
 contract MixEth is ERC223ReceivingContract {
@@ -22,21 +22,17 @@ contract MixEth is ERC223ReceivingContract {
 
   uint256 public amt = 1000000000000000000; //1 ether in wei, the amount of ether to be mixed;
   uint256 public shufflingDeposit = 1000000000000000000; // 1 ether, TBD
-  mapping(address => Shuffler) public shuffleRound; // we only store the parity of the shuffle round! false -> 0, true -> 1 and additionally the shuffler's address
-  mapping(address => Status) public shufflers;
-  mapping(address => mapping(bool => Shuffle)) public Shuffles;
+  mapping(address => bool) public shuffleRound; //token address to the parity of the round. we only store the parity of the shuffle round! false -> 0, true -> 1
+  mapping(address => Status) public shufflers; //shuffler address to their state
+  mapping(address => mapping(bool => Shuffle)) public Shuffles; //token address to round to shuffle
 
   /*
   describes a shuffle: contains the shuffled pubKeys and shuffling accumulated constant
   */
   struct Shuffle {
-    uint256[] shuffle;
+    mapping(uint256 => bool) shuffle; //whether a particular point is present in the shuffle or not
     address shuffler;
-  }
-
-  struct Shuffler {
-    bool turn;
-    address shufflerAddress;
+    uint256 noOfPoints; //note that one of these points is always the shuffling accumulated constant
   }
 
   struct Status {
@@ -45,6 +41,7 @@ contract MixEth is ERC223ReceivingContract {
   }
 
   event newShuffle(address indexed token, bool actualRound, address shuffler, uint256[] shuffle);
+  event successfulChallenge(address indexed token, bool actualRound, address shuffler);
 
   function () public {
     revert();
@@ -53,8 +50,9 @@ contract MixEth is ERC223ReceivingContract {
   function depositEther(uint256 initPubKeyX, uint256 initPubKeyY) public payable {
     require(msg.value == amt, "Ether denomination is not correct!");
     require(EC.onCurve([initPubKeyX, initPubKeyY]), "Invalid public key!");
-    Shuffles[0x0][shuffleRound[0x0].turn].shuffle.push(initPubKeyX);
-    Shuffles[0x0][shuffleRound[0x0].turn].shuffle.push(initPubKeyY);
+    Shuffles[0x0][shuffleRound[0x0]].shuffle[initPubKeyX] = true;
+    Shuffles[0x0][shuffleRound[0x0]].shuffle[initPubKeyY] = true;
+    Shuffles[0x0][shuffleRound[0x0]].noOfPoints = Shuffles[0x0][shuffleRound[0x0]].noOfPoints.add(1);
   }
 
   /*
@@ -68,21 +66,38 @@ contract MixEth is ERC223ReceivingContract {
     }
     require(token != 0 && codeLength > 0);
     require(EC.onCurve([initPubKeyX, initPubKeyY]), "Invalid public key!");
-    Shuffles[token][shuffleRound[token].turn].shuffle.push(initPubKeyX);
-    Shuffles[token][shuffleRound[token].turn].shuffle.push(initPubKeyY);
+    Shuffles[token][shuffleRound[token]].shuffle[initPubKeyX] = true;
+    Shuffles[token][shuffleRound[token]].shuffle[initPubKeyY] = true;
+    Shuffles[token][shuffleRound[token]].noOfPoints = Shuffles[token][shuffleRound[token]].noOfPoints.add(1);
 
     ERC20Compatible untrustedErc20Token = ERC20Compatible(token);
     untrustedErc20Token.transferFrom(msg.sender, this, 100);
-}
+  }
 
-  function uploadShuffle(address token, uint256[] _shuffle) public payable {
+  /*
+    @param address token: refers to the token address shuffler wants to shuffle
+    @param uint256[] _oldShuffle: refers to the last but one to-be-deleted shuffle
+    @param uint256[] _shuffle: the new to-be-uploaded shuffle
+  */
+  function uploadShuffle(address token, uint256[] _oldShuffle, uint256[] _shuffle) public payable {
     require(msg.value == shufflingDeposit, "Invalid shuffler deposit amount!");
     require(!shufflers[msg.sender].alreadyShuffled, "Shuffler is not allowed to shuffle more than once!");
-    Shuffles[token][!shuffleRound[token].turn] = Shuffle(_shuffle, msg.sender);
-    shuffleRound[token].turn = !shuffleRound[token].turn;
+    require(_oldShuffle.length == Shuffles[token][!shuffleRound[token]].noOfPoints, "Incorrectly referenced the last but one shuffle");
+    // remove the last but one shuffler
+    for(uint256 i = 0; i < _oldShuffle.length; i++) {
+      require(Shuffles[token][!shuffleRound[token]].shuffle[_oldShuffle[i]]);
+      Shuffles[token][!shuffleRound[token]].shuffle[_oldShuffle[i]] = false;
+    }
+    //upload new shuffle
+    for(i = 0; i < _shuffle.length; i++) {
+      Shuffles[token][!shuffleRound[token]].shuffle[_shuffle[i]] = true;
+    }
+    Shuffles[token][!shuffleRound[token]].shuffler = msg.sender;
+    Shuffles[token][!shuffleRound[token]].noOfPoints = (_shuffle.length)/2;
+    shuffleRound[token] = !shuffleRound[token];
     shufflers[msg.sender].alreadyShuffled = true; // a receiver can only shuffle once
 
-    emit newShuffle(token, !shuffleRound[token].turn, msg.sender, _shuffle);
+    emit newShuffle(token, !shuffleRound[token], msg.sender, _shuffle);
   }
 
   /*
@@ -90,62 +105,46 @@ contract MixEth is ERC223ReceivingContract {
     which is stored at Shuffles[round].
     If challenge accepted malicious shuffler's deposit is slashed.
   */
-  function challengeShuffle(uint256[22] proofTranscript, bool round, address token, uint256 indexinPrevShuffleA, uint256 indexinPrevShuffleA2) public {
-    uint256 prevShuffleLength = Shuffles[token][round].shuffle.length;
-    uint256 currentShuffleLength = Shuffles[token][!round].shuffle.length;
-    require(proofTranscript[0] == Shuffles[token][round].shuffle[prevShuffleLength-2] && proofTranscript[1] == Shuffles[token][round].shuffle[prevShuffleLength-1], "Wrong shuffling accumulated constant for previous round "); //checking correctness of C*_{i-1}
-    require(proofTranscript[2] == Shuffles[token][round].shuffle[indexinPrevShuffleA] && proofTranscript[3] == Shuffles[token][round].shuffle[indexinPrevShuffleA2], "Shuffled key is not included in previous round"); //checking that shuffled key is indeed included in previous shuffle
-    require(proofTranscript[4] == Shuffles[token][!round].shuffle[currentShuffleLength-2] && proofTranscript[5] == Shuffles[token][!round].shuffle[currentShuffleLength-1], "Wrong current shuffling accumulated constant"); //checking correctness of C*_{i}
-    bool includedInShuffle;
-    for(uint256 i=0; i < (currentShuffleLength-2)/2; i++) {
-      if(proofTranscript[6] == Shuffles[token][!round].shuffle[2*i] && proofTranscript[7] == Shuffles[token][!round].shuffle[2*i+1]) {
-        includedInShuffle = true;
-        break;
-      }
-    }
-    require(!includedInShuffle && ChaumPedersenVerifier.verifyChaumPedersen(proofTranscript), "Chaum-Pedersen Proof not verified");
-    shufflers[Shuffles[token][!round].shuffler].slashed = true;
-    shuffleRound[token].turn = !shuffleRound[token].turn;
-    //delete the shuffle entirely
-    for(i=0; i < (currentShuffleLength-2)/2; i++) {
-      Shuffles[token][!round].shuffle[2*i] = 0;
-      Shuffles[token][!round].shuffle[2*i+1] = 0;
-    }
+  function challengeShuffle(uint256[22] proofTranscript, address token) public {
+    bool round = shuffleRound[token]; //only current shuffles can be challenged
+    require(Shuffles[token][!round].shuffle[proofTranscript[0]] && Shuffles[token][!round].shuffle[proofTranscript[1]], "Wrong shuffling accumulated constant for previous round "); //checking correctness of C*_{i-1}
+    require(Shuffles[token][!round].shuffle[proofTranscript[2]] && Shuffles[token][!round].shuffle[proofTranscript[3]], "Shuffled key is not included in previous round"); //checking that shuffled key is indeed included in previous shuffle
+    require(Shuffles[token][round].shuffle[proofTranscript[4]] && Shuffles[token][round].shuffle[proofTranscript[5]], "Wrong current shuffling accumulated constant"); //checking correctness of C*_{i}
+    require(!Shuffles[token][round].shuffle[proofTranscript[6]] || !Shuffles[token][round].shuffle[proofTranscript[7]], "Final public key is indeed included in current shuffle");
+    require(ChaumPedersenVerifier.verifyChaumPedersen(proofTranscript), "Chaum-Pedersen Proof not verified");
+    shufflers[Shuffles[token][round].shuffler].slashed = true;
+    shuffleRound[token] = !shuffleRound[token];
+
+    emit successfulChallenge(token, round, Shuffles[token][round].shuffler);
   }
 
   //receivers can withdraw funds at most once
-  function withdrawAmt(uint256[12] sig, address token, uint256 indexInShuffle) public {
-    withdrawChecks(sig, token, indexInShuffle);
+  function withdrawAmt(uint256[12] sig, address token) public {
+    withdrawChecks(sig, token);
 
     msg.sender.transfer(amt);
   }
 
-
-  function withdrawERC20Compatible(uint256[12] sig, address token, uint256 indexInShuffle) public {
-    withdrawChecks(sig, token, indexInShuffle);
+  function withdrawERC20Compatible(uint256[12] sig, address token) public {
+    withdrawChecks(sig, token);
 
     ERC20Compatible untrustedErc20Token = ERC20Compatible(token);
     untrustedErc20Token.transfer(msg.sender, 100); //to-be-overwritten TODO
    }
 
-   function withdrawChecks(uint256[12] sig, address token, uint256 indexInShuffle) internal {
-     uint256 currentShuffleLength = Shuffles[token][shuffleRound[token].turn].shuffle.length;
-     require(Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle] == sig[2] && Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle+1] == sig[3], "Your public key is not included in the final shuffle!"); //public key is included in Shuffled
-     require(Shuffles[token][shuffleRound[token].turn].shuffle[currentShuffleLength-2] == sig[0] && Shuffles[token][shuffleRound[token].turn].shuffle[currentShuffleLength-1] == sig[1], "Your signature is using a wrong generator!"); //shuffling accumulated constant is correct
-     require(sig[4] == uint(sha3(msg.sender,sig[2],sig[3])), "Signed an invalid message!"); //this check is needed to deter front-running attacks
+   function withdrawChecks(uint256[12] sig, address token) internal {
+     require(Shuffles[token][shuffleRound[token]].shuffle[sig[2]] && Shuffles[token][shuffleRound[token]].shuffle[sig[3]], "Your public key is not included in the final shuffle!"); //public key is included in Shuffled
+     require(Shuffles[token][shuffleRound[token]].shuffle[sig[0]] && Shuffles[token][shuffleRound[token]].shuffle[sig[1]], "Your signature is using a wrong generator!"); //shuffling accumulated constant is correct
+     require(sig[4] == uint(sha3(msg.sender, sig[2], sig[3])), "Signed an invalid message!"); //this check is needed to deter front-running attacks
      require(ECDSAGeneralized.verify(sig), "Your signature is not verified!");
-     Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle] = 0;
-     Shuffles[token][shuffleRound[token].turn].shuffle[indexInShuffle+1] = 0;
+     Shuffles[token][shuffleRound[token]].shuffle[sig[2]] = false;
+     Shuffles[token][shuffleRound[token]].shuffle[sig[3]] = false;
+     Shuffles[token][shuffleRound[token]].noOfPoints = Shuffles[token][shuffleRound[token]].noOfPoints.sub(1);
    }
 
   function withdrawDeposit() public onlyShuffler onlyHonestShuffler {
     shufflers[msg.sender].slashed = true; //we only allow to withdraw shuffler deposits once
     msg.sender.transfer(shufflingDeposit);
-  }
-
-  // Shuffled pubKeys in shuffleRound
-  function getShuffle(address token, bool round) public view returns (uint256[] pubKeys) {
-    return Shuffles[token][round].shuffle;
   }
 
   modifier onlyShuffler() {
--- test/TestMixEth.js
+++ test/TestMixEth.js
@@ -7,12 +7,12 @@ let Web3latest = require('web3');
 let web3latest = new Web3latest();
 web3latest.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
+let pubKeyX = '0x1ec64fdd678f8528c981fbdd742e8c79fa91c6f7bfdfbebf2f99f74df6f09589';
+let pubKeyY = '0x118caf99d37bd0f75cd9efa455261f8806c14bae4ddee43690aff3bc1b6eef48';
 
-contract('MixEth', function(accounts) {
+contract('MixEth Deposits', function(accounts) {
     let ContractInstance;
     let shuffler = accounts[0];
-    let pubKeyX = '0x1ec64fdd678f8528c981fbdd742e8c79fa91c6f7bfdfbebf2f99f74df6f09589';
-    let pubKeyY = '0x118caf99d37bd0f75cd9efa455261f8806c14bae4ddee43690aff3bc1b6eef48';
 
     it("Depositting to a public key", function() {
         return MixEth.deployed().then(async function(instance) {
@@ -22,12 +22,10 @@ contract('MixEth', function(accounts) {
             pubKeyY,
             {value:1000000000000000000});
             console.log("Deposit tx gas cost:", txReceipt.receipt.gasUsed);
-            return ContractInstance.getShuffle(0x0000000000000000000000000000000000000000, false);
-        }).then(function(pubKey) {
-            let xcoordinate = new BigNumber(pubKey[0]).toString(16)
-            let ycoordinate = new BigNumber(pubKey[1]).toString(16)
-            assert.equal(xcoordinate, '1ec64fdd678f8528c981fbdd742e8c79fa91c6f7bfdfbebf2f99f74df6f09589', "X coordinate is not correct");
-            assert.equal(ycoordinate, '118caf99d37bd0f75cd9efa455261f8806c14bae4ddee43690aff3bc1b6eef48', "Y coordinate is not correct");
+            return ContractInstance.Shuffles(0x0000000000000000000000000000000000000000, false);
+        }).then(function(isDeposited) {
+            let noOfPoints = new BigNumber(isDeposited[1]).toString(16)
+            assert.equal(noOfPoints, 1, "Public key is not deposited");
         });
     });
 
@@ -50,15 +48,21 @@ contract('MixEth', function(accounts) {
               {value:1000000000000000000}));
         });
     });
+  });
+
+contract('MixEth Challenge', function(accounts) {
+      let ContractInstance;
+      let shuffler = accounts[0];
 
      it("Challenging a shuffle", function() {
          return MixEth.deployed().then(async function(instance) {
              ContractInstance = instance;
-             let a = await ContractInstance.depositEther(pubKeyX, pubKeyY,
+             /*let a = await ContractInstance.depositEther(pubKeyX, pubKeyY,
              {value:1000000000000000000, from:accounts[0]});
              let b = await ContractInstance.depositEther(pubKeyX, pubKeyY,
-             {value:1000000000000000000, from:accounts[1]});
+             {value:1000000000000000000, from:accounts[1]});*/
              let tx = await ContractInstance.uploadShuffle('0x0000000000000000000000000000000000000000',
+             [],
              ['0xd3b0b6d59fdd841d28821171aa912d625169d58fac04592b239bcab9b84082ee','0x99f88955859c34dc7c2cbe0ce423ded7e1d2302fc279be3f805f71dde7eca582',
              '0xf924738fbaebf80ca5a0d1d6cc33856be448c0e60670c64f73aeac0000925808','0xe392c39c052599470fea0142bea6add7cceb5a2f99423bb45320d35f083d4f60',
              '0x6c709bf8f273b07791824b08e1b99ece69c0fb5fbdcfe7c1150b0ccd7a816c54','0x21753728d8b3028f041a71eee89c7424a870c2ff8da41350cc15ef397df0d373',
@@ -68,6 +72,7 @@ contract('MixEth', function(accounts) {
              {value:1000000000000000000, from:accounts[0]});
              console.log("Gas cost of uploading a shuffle", tx.receipt.gasUsed);
              let abd = await ContractInstance.uploadShuffle('0x0000000000000000000000000000000000000000',
+             [],
              ['0x64a9a262658c9d2481b6f2d8eb4576affb8ae330fb0d9750901f3e110330dbc3', '0x241a4fbad8c75abfd00ed5f46a36620ca63adcc71be9b6d487a2c3ec1b968ebb',
              '0x8c46882be46a4e65cfd5515f8809253654e61a7fa4135d269a1694d32f374602', '0xe8675d17c8ed23bd205fa65ab505792cd16d3cffc1f2f347ae52040d77ce0848',
              '0xb5a86dc8d8b53b77ecc08bf6d0b0c8c25aae3ba843d59a78f460694ecadf4c85', '0xb252007a5ff554be11380b8d89f81564f78d57847c9f08ae55274c1897b2ab17',
@@ -87,9 +92,7 @@ contract('MixEth', function(accounts) {
               '0x3c6fc99cb6a3861f987fd7b0403dd184977f32dbea8f46b75a0c49d3d5ef705c','0x8812a011a0df83df4cf7732775ad026a24db180a7f4af7e424c1d04bb1635de8',
               '0xcd7a64b4afa73602d19fe83f1fbf6543d67f7e3c0ccd2f79497d9d999d2303b5','0x25fc314f21964df9bc46707355347676a9cd060599d5bde52478de49d0d6e421',
               '0xcc4d05dc508d875033ec31804ce0eef81dfe681d3a1594eeb4e393f8bd95eafb','0xba800929d95fab0d049cdf1631dd4c421630df8e39c12d0c83201db897d4c1ef'],
-              true,
-              '0x0000000000000000000000000000000000000000',
-              0,1
+              '0x0000000000000000000000000000000000000000'
              );
              console.log("challengeShuffle gas usage", txReceipt.receipt.gasUsed);
              return ContractInstance.shufflers.call(accounts[1]);
@@ -97,6 +100,12 @@ contract('MixEth', function(accounts) {
              assert.equal(true, shuffled[1], "Shuffler is not slashed!");
            });
       });
+
+    });
+
+    contract('MixEth Withdraw', function(accounts) {
+          let ContractInstance;
+          let shuffler = accounts[0];
 
       /*
         uint256 Gx, uint256 Gy, uint256 pKx, uint256 pKy,
@@ -107,9 +116,8 @@ contract('MixEth', function(accounts) {
       it("Withdrawing from the mixer", function() {
           return MixEth.deployed().then(async function(instance) {
               ContractInstance = instance;
-              let a = await ContractInstance.depositEther(pubKeyX, pubKeyY,
-              {value:1000000000000000000, from:accounts[0]});
               let tx = await ContractInstance.uploadShuffle('0x0000000000000000000000000000000000000000',
+              [],
               ['0xd3b0b6d59fdd841d28821171aa912d625169d58fac04592b239bcab9b84082ee','0x99f88955859c34dc7c2cbe0ce423ded7e1d2302fc279be3f805f71dde7eca582',
               '0xf924738fbaebf80ca5a0d1d6cc33856be448c0e60670c64f73aeac0000925808','0xe392c39c052599470fea0142bea6add7cceb5a2f99423bb45320d35f083d4f60',
               '0x6c709bf8f273b07791824b08e1b99ece69c0fb5fbdcfe7c1150b0ccd7a816c54','0x21753728d8b3028f041a71eee89c7424a870c2ff8da41350cc15ef397df0d373',
@@ -124,11 +132,12 @@ contract('MixEth', function(accounts) {
               '0xac402dfc8929141090a320508de763d020fedbe00286cde14673e173d691fc8f','0x8d8684b3edd169d9eefcc2732bceb5baca902476fd8e1a4dd14010a6845d3f68',
               '0xe4237819aef1db170ffe8a9e768d623f95c7b2a5f0ad717be86fbfbdf9bd679e','0xe8b71a0ee31ec3cd54af6c926943dc5986c510392f8b6b9cfce2113f9e108045',
               '0xc718652f3c3814ef324d636312e4558a21232cb806f15cccf60125ecbf91c35c'],
-              '0x0000000000000000000000000000000000000000',8, {from:accounts[2]});
+              '0x0000000000000000000000000000000000000000', {from:accounts[2]});
               console.log("Gas cost of witdhrawing from the mixer", tx2.receipt.gasUsed);
-              return ContractInstance.getShuffle('0x0000000000000000000000000000000000000000', false);
+              return ContractInstance.Shuffles('0x0000000000000000000000000000000000000000', true);
             }).then(function(a){
-              console.log("Last shuffle:",a);
+              let noOfPoints = new BigNumber(a[1]).toString(16)
+              assert.equal(5, noOfPoints, "No of shuffled keys is not decreased after withdrawal");
             });
         });
   });
