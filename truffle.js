642e05bff79f914948df73a975f85f94cf6de64d
Seres István András <seresistvanandras@gmail.com>
2018-12-20 10:51:10 +0100

    Short paper update

--- truffle.js
+++ truffle.js
@@ -1,17 +1,4 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
- let HDWalletProvider = require('truffle-hdwallet-provider');
+let HDWalletProvider = require('truffle-hdwallet-provider');
 let mnemonic = 'dream pilot judge begin office silent improve scatter grant dream hollow arrive';
 
 
